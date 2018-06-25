10b2031180b9f55021ed63ca1143a6210cd41324
Dominic Tremblay <dominictremblay24@gmail.com>
2018-06-25 18:59:02 -0400

    Added Migration: Create Orders

---
+++ migrations/20180625185747_createOrders.js
@@ -0,0 +1,32 @@
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable("orders", table => {
+      table.increments("id").primary();
+      table.date("order_date");
+      table.string("shipVia");
+    }),
+
+    knex.schema.createTable("order_details", table => {
+      table.integer("order_id");
+      table.integer("product_id");
+      table.primary(["order_id", "product_id"]);
+      table.decimal("unit_price", 14, 2);
+      table.integer("quantity");
+      table
+        .foreign(["order_id", "product_id"])
+        .references(["orders.id", "products.id"]);
+    })
+  ]);
+};
+
+// table.foreign(['colA','colB'])
+//     .references(['colA','colB'])
+//     .on('tableB');
+
+// watch for the order of the drop tables
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable("order_details"),
+    knex.schema.dropTable("orders")
+  ]);
+};
